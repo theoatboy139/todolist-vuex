41f1213609cc03b8e3885453aa216e7b36e51a03
Tanawit <tanawit139@gmail.com>
2019-06-07 16:39:09 +0700

    second push

---
+++ src/components/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h1 font: Arial>Todo Table List</h1>
+        <ul>
+            <li v-for='(task,index) in renderTasks' :key='index'>
+                {{task}}
+                <button type = "button" width:auto height:auto @click="removeTask(index)">Remove</button>
+                <!-- <button type = "button" width:auto height:auto @click="setTaskDone(index)">Done</button> -->
+            </li>
+        </ul>
+        <input type="text" width="20" height="5" v-model="newTask" @keyup.enter="addTask()">
+        <button type="button" width = "20" height = "5" @click="addTask()">Add task</button>
+    </div>      
+</template>
+
+
+
+<script>
+export default {
+    data() {
+        return {
+            newTask : ""
+        }   
+    },
+
+    computed: {
+        renderTasks(){
+            return this.$store.getters.todoTasks
+        }
+    },
+
+    methods:{
+        addTask : function() {
+          this.$store.dispatch('addNewTask',this.newTask)
+          this.newTask = ""
+        },
+
+        removeTask : function(index) {
+            this.$store.dispatch('removeTask',index)
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,34 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    tasks : ["test","vuex","todo list"]
   },
+
+  getters:{
+    todoTasks (state) {
+      return state.tasks
+    }
+  },
+
   mutations: {
+    addNewTask (state, newTask) {
+      state.tasks = [...state.tasks,newTask]  
+    },
 
+    removeTask (state, index){
+      state.tasks = state.tasks.filter((task,i) => {
+        return i != index
+      })
+    }
   },
-  actions: {
 
+  actions: {
+    addNewTask (context, newTask){
+      context.commit('addNewTask', newTask)
+    },
+
+    removeTask (context, index){
+      context.commit('removeTask', index)
+    }
   }
 })
